ef8c834414cf955c131a3a1bab8beab77ba1559f
Lucas Matias Almendros <lucas.almendros@globant.com>
2019-09-03 14:40:51 -0300

    I can retrieve part of the log of any user by also passing along optional parameters of from & to or limit. (Date format yyyy-mm-dd, limit = int)

--- controllers/user.js
+++ controllers/user.js
@@ -1,68 +1,75 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.addUser = (req, res) => {
-    let user = new User(
-        {
-            username: req.body.username
-        }
-    );
+  let user = new User({
+    username: req.body.username
+  });
 
-    user.save(function (err, user) {
-        if (err) {
-            if (err.code === 11000) {
-                res.send("username already taken");
-            } else {
-                throw err;
-            }
-        } else {
-            res.json(user);
-        }
-    })
+  user.save(function(err, user) {
+    if (err) {
+      if (err.code === 11000) {
+        res.send("username already taken");
+      } else {
+        throw err;
+      }
+    } else {
+      res.json(user);
+    }
+  });
 };
 
 exports.getAllUsers = (req, res) => {
-    User.find().select('_id username').exec((err, users) => {
-        if (err) throw err;
-        res.json(users);
-    })
-}
+  User.find()
+    .select("_id username")
+    .exec((err, users) => {
+      if (err) throw err;
+      res.json(users);
+    });
+};
 
 exports.addExercise = (req, res, next) => {
-    const date = req.body.date ? req.body.date : undefined;
-    const exercise = { description: req.body.description, duration: req.body.duration, date: date };
-    User.findOneAndUpdate(
-        { _id: req.body.userId },
-        { $push: { exercises: exercise } },
-        function (err, success) {
-            if (err) return next({ status: 400, message: err.reason.message });
-            res.json(success);
-        }
-    );
+  const date = req.body.date ? req.body.date : undefined;
+  const exercise = {
+    description: req.body.description,
+    duration: req.body.duration,
+    date: date
+  };
+  User.findOneAndUpdate(
+    { _id: req.body.userId },
+    { $push: { exercises: exercise } },
+    function(err, success) {
+      if (err) return next({ status: 400, message: err.reason.message });
+      res.json(success);
+    }
+  );
 };
 
 exports.getLog = (req, res, next) => {
-    let query = User.findById(req.query.userId)
+  if (!req.query.userId) return next({ status: 400, message: "miss userID" });
+  let query = User.findById(req.query.userId);
+
+  if (req.query.limit) {
+    query.slice("exercises", parseInt(req.query.limit));
+  }
+  query.exec((err, respDB) => {
+    if (err) {
+      console.log(err);
+      return next({ status: 400, message: err.reason.message });
+    }
+    if (!respDB) return next({ status: 200, message: "not log" });
     if (req.query.from) {
-        var as = new Date(req.query.from)
-        query.where('exercises.date').gte(as.toJSON());
+      const from = new Date(req.query.from);
+      respDB.exercises = respDB.exercises.filter(item => item.date > from);
     }
     if (req.query.to) {
-        console.log(new Date(req.query.to))
-        query.where('exercises.date').lte(req.query.to);
+      const to = new Date(req.query.to);
+      respDB.exercises = respDB.exercises.filter(item => item.date < to);
     }
-    if (req.query.limit) {
-        query.slice('exercises', parseInt(req.query.limit))
-    }
-    query.exec((err, respDB) => {
-        if (err) {
-            console.log(err);
-            return next({ status: 400, message: err.reason.message })
-        };
-        res.json({
-            _id: respDB._id,
-            username: respDB.username,
-            count: respDB.exercises.length,
-            log: respDB.exercises
-        });
-    })
-}
+    res.json({
+      _id: respDB._id,
+      username: respDB.username,
+      count: respDB.exercises.length,
+      log: respDB.exercises
+    });
+  });
+};
